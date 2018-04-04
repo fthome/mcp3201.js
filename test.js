1e4c0b7f043071838490a821606a00a3636c4d5b
fthome <37829675+fthome@users.noreply.github.com>
2018-04-04 16:43:49 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,12 +2,12 @@
 This is just a dumb test to check that the MCP3008 works. It requires the ADC to be wired up and have something connected to channel 0. Edit as necessary to suit your setup.
 */
 
-var Mcp3008 = require('./mcp3008'),
-    adc = new Mcp3008(),
+var Mcp3201 = require('./mcp2101'),
+    adc = new Mcp3201(),
     out = function (value) {
         console.log("Read", value);
     };
 
-adc.read(0, out);
-adc.poll(0, 50, out);
+adc.read(out);
+adc.poll(50, out);
 setTimeout(function () { adc.close(); }, 1000);
